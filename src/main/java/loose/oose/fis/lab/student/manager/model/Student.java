3884940cbfeba01aad2bc371d632d4edcd6e2fb5
Marian <croitor140@gmail.com>
2025-03-26 15:18:24 +0200

    SM-2 implemented the Student model class

---
+++ src/main/java/loose/oose/fis/lab/student/manager/model/Student.java
@@ -0,0 +1,28 @@
+package loose.oose.fis.lab.student.manager.model;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double meanGrade;
+
+    public Student(String firstName, String lastName, int age, double meanGrade) {
+        //assignments
+    }
+
+    @Override
+    public String toString() {
+        return "";
+        //toString generated implementation
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+    return 23;
+    }
+}
